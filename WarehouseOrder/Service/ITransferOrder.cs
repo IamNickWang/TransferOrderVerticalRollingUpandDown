15389ccc80463181654161c77a56c35db357f31c
Nick Wang <nickwang33@hotmail.com>
2025-04-07 13:09:45 -0700

    Add files via upload

---
+++ WarehouseOrder/Service/ITransferOrder.cs
@@ -0,0 +1,9 @@
+﻿using WarehouseOrder.Model;
+
+namespace WarehouseOrder.Service
+{
+    public interface ITransferOrder
+    {
+        Task<List<erpTransferOrder>> GetTransferOrders();
+    }
+}
---
+++ WarehouseOrder/Service/TransferOrderService.cs
@@ -0,0 +1,26 @@
+﻿using WarehouseOrder.Model;
+
+namespace WarehouseOrder.Service
+{
+    public class TransferOrderService : ITransferOrder
+    {
+        private readonly DatabaseContext _dbt;
+        private readonly IConfiguration _config;
+
+
+        public TransferOrderService(DatabaseContext ctx, IConfiguration config)
+        {
+            _dbt = ctx;
+            _config = config;
+        }
+        public async Task<List<erpTransferOrder>> GetTransferOrders()
+        {
+            string _conn = _config.GetConnectionString("conn");
+
+            string sql = "SELECT * FROM erp.netsuiteTransferOrder";
+
+            return await _dbt.LoadData<erpTransferOrder>(sql, _conn);
+
+        }
+    }
+}
